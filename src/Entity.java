fb092fdcaea2e9d314fe0477dc2f7865bba06588
Alexsey <alexseyprimak@gmail.com>
2023-03-26 19:26:45 +0200

    added cat and Entity

---
+++ src/Entity.java
@@ -0,0 +1,19 @@
+public abstract class Entity {
+
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity{" +
+                "id=" + id +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cat cat = new Cat("name", 5);
+        System.out.println(cat);
     }
 }
